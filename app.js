1da3babd64d1875b0c2b80615748cf6ac39327ae
okamotodai <77658919+okamotodai@users.noreply.github.com>
2021-01-20 17:21:20 +0900

    Delete app.js

--- app.js
+++
@@ -1,7 +0,0 @@
-var express = require('express');
-var app = express();
-app.use(express.static('./'));
-var port = 3000;
-app.listen(port,function(){
-	console.log("Expressサーバーがポート%dで起動しました。モード:%s",port,app.settings.env)
-});

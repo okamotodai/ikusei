744586b225f60d6370913385594e6e691fa28004
okamotodai <77658919+okamotodai@users.noreply.github.com>
2021-01-29 07:21:18 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -15,8 +15,11 @@ app.get('/', (request, response) => {
   response.json({ info: 'Node.js, Express, and Postgres API' })
 })
 
-app.get('/', db.getUsers)
-app.get('/:id', db.getUserById)
+app.get('/users', db.getUsers)
+app.get('/users/:id', db.getUserById)
+app.post('/users', db.createUser)
+app.put('/users/:id', db.updateUser)
+app.delete('/users/:id', db.deleteUser)
 
 app.listen(port, () => {
   console.log(`App running on port ${port}.`)
